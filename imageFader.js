4bf682acacdfa454809afb33a61401f932918f65
Stephen Sylvia <ssylvia@esri.com>
2012-09-11 16:00:27 -0400

    redesigned with divs not ul

--- imageFader.js
+++ imageFader.js
@@ -1,302 +1,165 @@
-(function( $ ) {
-  $.fn.imageFader = function(method) {
+(function( $ ){
+    $.fn.imageFader = function( method ) {
 
-    // Method calling logic
-    if ( methods[method] ) {
-		return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	} else if ( typeof method === 'object' || ! method ) {
-		return methods.init.apply( this, arguments );
-	} else {
-		$.error( 'Method ' +  method + ' does not exist in MultiTips' );
-	}
+        if ( methods[method] ) {
+        	return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    	} else if ( typeof method === 'object' || ! method ) {
+    		return methods.init.apply( this, arguments );
+    	} else {
+    		$.error( 'Method ' +  method + ' does not exist in imageFader' );
+    	}
 
-  };
+    };
 
-  var settings,
-      animationReady = true;
+    var methods = {
 
-  var methods = {
+        init : function(options){
+            return this.each(function(options){
 
-      init : function(options){
-          initImageFader(this,options);
-      },
-      pause : function(){
-          pauseAnimation(this);
-      },
-      play : function(){
-          playAnimations(this);
-      },
-      next : function(){
-          nextImg(this);
-      },
-      prev : function(){
-          prevImg(this);
-      },
-      goTo : function(pos){
-          goToPos(this,pos);
-      },
-      update : function(){
-          updateImageFader(this);
-      }
+                //Default Options
+                var settings = $.extend({
+                    "startPosition" : 0,
+                    "autoPlay" : true,
+                    "animationSpeed" : 500,
+                    "animationDelay" : 5000,
+                    "captions" : false,
+                    "captionAttr" : "title",
+                    "animationStart" : function(eventObj){},
+                    "animationEnd" : function(eventObjt){}
+                },options);
 
-  };
+                var data = {
+                    "settings" : settings
+                };
 
-  var initImageFader = function(fader,options){
+                $(this).data("imageFader", data);
 
-      //Default Options
-      settings = $.extend({
-          "startPosition" : 0,
-          "autoPlay" : true,
-          "animationSpeed" : 500,
-          "animationDelay" : 4000,
-          "captions" : false,
-          "captionAttr" : "title",
-          "animationStart" : function(eventObj){},
-          "animationEnd" : function(eventObjt){}
+                initializeFader($(this));
 
-      },options);
+            });
+        },
+        destroy : function(){
+            return this.each(function(){
 
-      checkImgLoad(fader,options);
+                $(this).data("imageFader", undefined);
+                $(window).unbind(".imageFader");
 
-      $(window).resize(function(){
-          var imageTags = fader.children("li").children("img");
-          imageTags.css("max-width",fader.width()).css("max-height",fader.height());
+            });
+        }
 
-          imageTags.each(function(){
-              if($(this).width() > 0 && $(this).parent().is(":visible")){
-                  $(this).css("margin-left",(fader.width() - $(this).width())/2).css("margin-top",(fader.height() - $(this).height())/2);
-              }
-              else if($(this).parent().is(":visible") === false){
-                  $(this).parent().show();
-                  $(this).css("margin-left",(fader.width() - $(this).width())/2).css("margin-top",(fader.height() - $(this).height())/2);
-                  $(this).parent().hide();
+    };
 
-              }
-              else{
-                  $(this).load(function(){
-                      $(this).css("margin-left",(fader.width() - $(this).width())/2).css("margin-top",(fader.height() - $(this).height())/2);
-                  });
-              }
-          });
-      });
+    var initializeFader = function(fader){
 
-  };
+        var data = fader.data("imageFader");
 
-  var pauseAnimation = function(fader){
-      settings.autoPlay = false;
-      fader.children("li").removeClass("playingAnimation");
-  };
+        fader.children("img").each(function(){
+            $(this).hide().addClass("imageFaderGalleryImg hiddenImg").css("max-height",fader.height()).css("max-width",fader.width());
+            $(this).css("position","absolute");
+        });
 
-  var playAnimations = function(fader){
-      settings.autoPlay = true;
-      startSlideShow(fader);
-      fader.children("li").addClass("playingAnimation");
-  };
+        if(fader.children("img").eq(data.settings.startPosition)[0].complete){
+            fader.children("img").eq(data.settings.startPosition).fadeIn(data.settings.animationSpeed).removeClass("hiddenImg").addClass("visibleImg");
+            repositionImg(fader,fader.children("img").eq(data.settings.startPosition));
+            setTimeout(function() {
+                if(data.settings.autoPlay === true){
+                    startSlideShow(fader);
+                }
+            }, data.settings.animationSpeed);
+        }
+        else{
+            fader.children("img").eq(data.settings.startPosition).load(function(){
+                $(this).fadeIn(data.settings.animationSpeed).removeClass("hiddenImg").addClass("visibleImg");
+                repositionImg(fader,$(this));
+                setTimeout(function() {
+                    if(data.settings.autoPlay === true){
+                        startSlideShow(fader);
+                    }
+                }, data.settings.animationSpeed);
+            });
+        }
 
-  var nextImg = function(fader){
-      pauseAnimation(fader);
-      startSlideShow(fader);
-      setTimeout(function() {
-          animationReady = true;
-      }, settings.animationSpeed);
-  };
+        data.currentImg = {"index" : data.settings.startPosition,
+            "src" : fader.children("img").eq(data.settings.startPosition).attr("src"),
+            "caption" : fader.children("img").eq(data.settings.startPosition).attr(data.settings.captionAttr),
+            "jqueryElement" : fader.children("img").eq(data.settings.startPosition)
+        };
 
-  var prevImg = function(fader){
-      pauseAnimation(fader);
-      fader.children("li").each(function(){
-          if($(this).hasClass("visibleImg")){
-              if($(this).index() === 0){
-                  if(fader.children("li").last().children("img").width > 0 && fader.children("li").last().children("img").is(":visible")){
-                      fadeFunction(fader.children("li").last(),$(this),fader);
-                  }
-                  else if(fader.children("li").last().children("img").is(":visible") === false){
-                      fadeFunction(fader.children("li").last(),$(this),fader);
-                  }
-                  else{
-                      fader.children("li").last().children("img").load(function(){
-                          fadeFunction(fader.children("li").last(),$(this),fader);
-                      });
-                  }
-              }
-              else{
-                  if($(this).prev().children("img").width > 0 && $(this).prev().children("img").is(":visible")){
-                      fadeFunction($(this).prev(),$(this),fader);
-                  }
-                  else if($(this).prev().children("img").is(":visible") === false){
-                      fadeFunction($(this).prev(),$(this),fader);
-                  }
-                  else{
-                      $(this).prev().children("img").load(function(){
-                          fadeFunction($(this).prev(),$(this),fader);
-                      });
-                  }
-              }
-          }
-      });
-      setTimeout(function() {
-          animationReady = true;
-      }, settings.animationSpeed);
-};
+        $(window).bind('resize.imageFader',function(){
+            $(".visibleImg").each(function(){
+                repositionImg(fader,$(this));
+            });
+        });
+    };
 
-  var goToPos = function(fader,pos){
-      if (pos > fader.children("li").length - 1) {
-          console.log("Error: No image at position " + pos);
-      }
-      else{
-          pauseAnimation(fader);
-          fader.children("li").each(function(){
-              if($(this).hasClass("visibleImg")){
-                  if(fader.children("li").eq(pos).children("img").width > 0 && fader.children("li").eq(pos).children("img").is(":visible")){
-                      fadeFunction(fader.children("li").eq(pos),$(this),fader);
-                  }
-                  else if(fader.children("li").eq(pos).children("img").is(":visible") === false){
-                      fadeFunction(fader.children("li").eq(pos),$(this),fader);
-                  }
-                  else{
-                      fader.children("li").eq(pos).children("img").load(function(){
-                          fadeFunction(fader.children("li").eq(pos),$(this),fader);
-                      });
-                  }
-              }
-          });
-          setTimeout(function() {
-              animationReady = true;
-          }, settings.animationSpeed);
-      }
-  };
+    var repositionImg = function(fader,img){
+        img.css("max-height",fader.height()).css("max-width",fader.width()).css("margin-left",(fader.width() - img.width())/2).css("margin-top",((fader.height() - img.height())/2));
+    };
 
-  var updateImageFader = function(fader){
-      $(".imageFaderCaption").remove();
-      fader.children("li").removeClass("hiddenImg").removeClass("visibleImg").show();
-      if(settings.autoPlay === true){
-          pauseAnimation(fader);
-          fader.children("li").addClass("playingAnimation");
-          checkImgLoad(fader);
-          setTimeout(function() {
-              if(fader.children("li").first().hasClass("playingAnimation")){
-                  settings.autoPlay = true;
-                  startSlideShow(fader);
-              }
-          }, settings.animationDelay);
-      }
-      else{
-          checkImgLoad(fader);
-      }
-      animationReady = true;
-  };
+    var startSlideShow = function(fader){
+        var data = fader.data("imageFader");
 
-  var checkImgLoad = function(fader,options){
-      var imageTags = fader.children("li").children("img");
+        if(data.currentImg.index === fader.children("img").length - 1){
+            if(fader.children("img").first()[0].complete){
+                setTimeout(function() {
+                        if(data.settings.autoPlay === true){
+                            fadeImages(fader,fader.children("img").first(),data.currentImg.jqueryElement);
+                        }
+                    }, data.settings.animationDelay);
+            }
+            else{
+                fader.children("img").first().load(function(){
+                    setTimeout(function() {
+                        if(data.settings.autoPlay === true){
+                            fadeImages(fader,$(this),data.currentImg.jqueryElement);
+                        }
+                    }, data.settings.animationDelay);
+                });
+            }
+        }
+        else{
+            if(data.currentImg.jqueryElement.next()[0].complete){
+                setTimeout(function() {
+                        if(data.settings.autoPlay === true){
+                            fadeImages(fader,data.currentImg.jqueryElement.next(),data.currentImg.jqueryElement);
+                        }
+                    }, data.settings.animationDelay);
+            }
+            else{
+                data.currentImg.jqueryElement.next().load(function(){
+                    setTimeout(function() {
+                        if(data.settings.autoPlay === true){
+                            fadeImages(fader,$(this),data.currentImg.jqueryElement);
+                        }
+                    }, data.settings.animationDelay);
+                });
+            }
+        }
+    };
 
-      imageTags.css("max-width",fader.width()).css("max-height",fader.height());
-      fader.children("li").css("position","absolute").css("padding","0").css("margin","0").css("width","100%").css("height","100%");
-      fader.css("list-style-type","none").css("padding","0").css("margin","0");
+    var fadeImages = function(fader,fIn,fOut){
+        var data = fader.data("imageFader");
 
-      if (settings.captions === true){
-          fader.children("li").each(function(i){
-              var caption = $(this).children("img").first().attr(settings.captionAttr);
-              $(this).append("<div id='imgCaption"+i+"' class='imageFaderCaption'>"+caption+"</div>");
-          });
-      }
+        data.settings.animationStart(data);
 
-      imageTags.each(function(){
-          $(this).parent().addClass("hiddenImg");
-          if($(this).width() > 0){
-              $(this).css("margin-left",(fader.width() - $(this).width())/2).css("margin-top",(fader.height() - $(this).height())/2);
-              if($(this).parent().index() != settings.startPosition){
-                  $(this).parent().hide();
-              }
-              else{
-                  $(this).parent().removeClass("hiddenImg");
-                  $(this).parent().addClass("visibleImg");
-                  if(settings.autoPlay === true){
-                      setTimeout(function() {
-                          startSlideShow(fader,options);
-                      }, settings.animationDelay);
-                  }
-              }
-          }
-          else{
-              $(this).load(function(){
-                  $(this).css("margin-left",(fader.width() - $(this).width())/2).css("margin-top",(fader.height() - $(this).height())/2);
-                  if($(this).parent().index() != settings.startPosition){
-                      $(this).parent().hide();
-                  }
-                  else{
-                      $(this).parent().removeClass("hiddenImg");
-                      $(this).parent().addClass("visibleImg");
-                      setTimeout(function() {
-                          if(settings.autoPlay === true){
-                              startSlideShow(fader,options);
-                          }
-                      }, settings.animationDelay);
-                  }
-              });
-          }
-      });
+        fOut.fadeOut(data.settings.animationSpeed);
+        fIn.fadeIn(data.settings.animationSpeed).removeClass("hiddenImg").addClass("visibleImg");
+        repositionImg(fader,fIn);
 
-  };
+        setTimeout(function() {
+            repositionImg(fader,fIn);
+            fOut.removeClass("visibleImg").addClass("hiddenImg");
 
-  var startSlideShow = function(fader,options){
-      fader.children("li").each(function(){
-          if(settings.autoPlay === true){
-              $(this).addClass("playingAnimation");
-          }
-          if($(this).hasClass("visibleImg")){
-              if($(this).index() === fader.children("li").length - 1){
-                  if(fader.children("li").first().children("img").width > 0 && fader.children("li").first().children("img").is(":visible")){
-                      fadeFunction(fader.children("li").first(),$(this),fader,options);
-                  }
-                  else if(fader.children("li").first().children("img").is(":visible") === false){
-                      fadeFunction(fader.children("li").first(),$(this),fader,options);
-                  }
-                  else{
-                      fader.children("li").first().children("img").load(function(){
-                          fadeFunction(fader.children("li").first(),$(this),fader,options);
-                      });
-                  }
-              }
-              else{
-                  if($(this).next().children("img").width > 0 && $(this).next().children("img").is(":visible")){
-                      fadeFunction($(this).next(),$(this),fader,options);
-                  }
-                  else if($(this).next().children("img").is(":visible") === false){
-                      fadeFunction($(this).next(),$(this),fader,options);
-                  }
-                  else{
-                      $(this).next().children("img").load(function(){
-                          fadeFunction($(this).next(),$(this),fader,options);
-                      });
-                  }
-              }
-          }
-      });
-  };
+            data.currentImg = {"index" : fIn.index(),
+                "src" : fIn.attr("src"),
+                "caption" : fIn.attr(data.settings.captionAttr),
+                "jqueryElement" : fIn
+            };
+            data.settings.animationEnd(data);
 
-  var fadeFunction = function(fIn,fOut,fader,options){
-      if(animationReady === true){
-          animationReady = false;
-          var eventObj = {"index" : fOut.index()};
-          settings.animationStart(eventObj);
-          fIn.fadeIn(settings.animationSpeed);
-          fOut.fadeOut(settings.animationSpeed);
-          setTimeout(function() {
-              eventObj = {"index" : fIn.index()};
-              settings.animationEnd(eventObj);
-              fIn.addClass("visibleImg");
-              fIn.removeClass("hiddenImg");
-              fOut.removeClass("visibleImg");
-              fOut.addClass("hiddenImg");
+            startSlideShow(fader);
 
-              setTimeout(function() {
-                  animationReady = true;
-                  if(settings.autoPlay === true){
-                      startSlideShow(fader,options);
-                  }
-              }, settings.animationDelay);
-
-          }, settings.animationSpeed);
-      }
-  };
+        }, data.settings.animationSpeed);
+    };
 
 })( jQuery );
